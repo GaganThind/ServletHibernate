75b4ea3624ff3b52e3427a9da98bdaf4903f4b37
Falcon <gaganthind277@gmail.com>
2016-05-18 22:08:11 +0530

    Added HibernateUtil.java

--- src/in/gagan/hibernate/dao/LoginDAO.java
+++ src/in/gagan/hibernate/dao/LoginDAO.java
@@ -8,23 +8,24 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Restrictions;
 
+import in.gagan.common.util.HibernateUtil;
 import in.gagan.hibernate.dto.UserLogin;
 
 public class LoginDAO {
-	public boolean Authenticate(String userName, String password){	 
-		 
-		 SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		 Session session = sessionFactory.openSession();
-		 session.beginTransaction();
-		 Criteria criteria = session.createCriteria(UserLogin.class);
-		 criteria.add(Restrictions.eq("userName", userName)).add(Restrictions.eq("password", password));
-		 List<UserLogin> user = (List<UserLogin>)criteria.list();
-		 session.getTransaction().commit();
-		 session.close();
-		 
-		 if(user.size()>0 && !user.isEmpty()){
-			 return true;
-		 }
+	public boolean Authenticate(String userName, String password) {
+
+		Session session = HibernateUtil.openSession();
+		session.beginTransaction();
+		Criteria criteria = session.createCriteria(UserLogin.class);
+		criteria.add(Restrictions.eq("userName", userName)).add(Restrictions.eq("password", password));
+		@SuppressWarnings("unchecked")
+		List<UserLogin> user = (List<UserLogin>) criteria.list();
+		session.getTransaction().commit();
+		session.close();
+
+		if (user.size() > 0 && !user.isEmpty()) {
+			return true;
+		}
 		return false;
-	} 
+	}
 }
